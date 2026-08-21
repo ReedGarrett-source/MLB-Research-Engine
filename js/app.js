eeb41b8026add9111021a736f844a70465c42a82
ReedGarrett-source <reedgarrettlover@gmail.com>
2026-08-21 15:13:10 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 const API_URL = "https://mlb-research-engine.onrender.com";
 
-
 async function askQuestion() {
   const question = document.getElementById("question").value;
   const answer = document.getElementById("answer");
@@ -24,27 +23,49 @@ async function askQuestion() {
       return;
     }
 
-answer.innerHTML = `
-  <strong>${data.player}</strong><br><br>
+    // Pitching results
+    if (data.ERA !== undefined) {
+      answer.innerHTML = `
+        <strong>${data.player}</strong><br><br>
 
-  Games: ${data.games}<br>
-  AB: ${data.atBats}<br>
-  H: ${data.hits}<br>
-  R: ${data.runs}<br>
-  2B: ${data.doubles}<br>
-  3B: ${data.triples}<br>
-  HR: ${data.homeRuns}<br>
-  RBI: ${data.rbi}<br>
-  BB: ${data.walks}<br>
-  K: ${data.strikeOuts}<br>
-  SB: ${data.stolenBases}<br>
-  CS: ${data.caughtStealing}<br><br>
+        Games: ${data.games}<br>
+        IP: ${data.inningsPitched}<br>
+        ER: ${data.earnedRuns}<br>
+        H: ${data.hitsAllowed}<br>
+        BB: ${data.walks}<br>
+        K: ${data.strikeOuts}<br>
+        HR: ${data.homeRuns}<br><br>
 
-  AVG: ${data.battingAverage}<br>
-  OBP: ${data.onBasePercentage}<br>
-  SLG: ${data.sluggingPercentage}<br>
-  <strong>OPS: ${data.OPS}</strong>
-`;
+        ERA: ${data.ERA}<br>
+        WHIP: ${data.WHIP}
+      `;
+
+      return;
+    }
+
+    // Hitting results
+    answer.innerHTML = `
+      <strong>${data.player}</strong><br><br>
+
+      Games: ${data.games}<br>
+      AB: ${data.atBats}<br>
+      H: ${data.hits}<br>
+      R: ${data.runs}<br>
+      2B: ${data.doubles}<br>
+      3B: ${data.triples}<br>
+      HR: ${data.homeRuns}<br>
+      RBI: ${data.rbi}<br>
+      BB: ${data.walks}<br>
+      K: ${data.strikeOuts}<br>
+      SB: ${data.stolenBases}<br>
+      CS: ${data.caughtStealing}<br><br>
+
+      AVG: ${data.battingAverage}<br>
+      OBP: ${data.onBasePercentage}<br>
+      SLG: ${data.sluggingPercentage}<br>
+      <strong>OPS: ${data.OPS}</strong>
+    `;
+
   } catch (error) {
     answer.textContent =
       "Unable to connect to the MLB Research Engine.";
