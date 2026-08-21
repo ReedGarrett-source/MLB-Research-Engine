70e4796b19ddf9ac628acbdeeb7a906f8dff1109
ReedGarrett-source <reedgarrettlover@gmail.com>
2026-08-21 00:36:11 -0400

    Update server.js

--- js/backend/server.js
+++ js/backend/server.js
@@ -1,1 +1,19 @@
-console.log("MLB Research Engine backend started.");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  });
+
+  res.end(
+    JSON.stringify({
+      message: "MLB Research Engine backend is working!"
+    })
+  );
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
